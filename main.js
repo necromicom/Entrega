6a5e4a61ffc78d0075cd45bef9f8b6296166d251
necromicom <ebrancatti@gmail.com>
2025-01-19 23:48:42 -0300

    p3

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ function login() {
             alert("Usuario inválido. Inténtelo nuevamente.");
             intentos++;
         } else if (user === "admin" || user === "user") {
-            
             let pass = prompt("Ingresar contraseña:");
 
             if (pass == null || pass === "") {
@@ -29,50 +28,117 @@ function login() {
 
         if (intentos > 3) {
             alert("Usted superó los 3 intentos. Intente de nuevo");
-            break;
+            identificar = false;
         }
     } while (identificar);
 }
 
-login();
+const Obj = function (name, pri, stock) {
+    this.name = name;
+    this.pri = pri;
+    this.stock = stock;
+};
 
+let o1 = new Obj("alcohol", 1500, 30);
+let o2 = new Obj("mascara facial", 15000, 10);
+let o3 = new Obj("crema para manos", 5000, 20);
+let o4 = new Obj("aposito", 500, 500);
+let o5 = new Obj("Gasa", 500, 1000);
+let o6 = new Obj("aguja", 600, 700);
+let o7 = new Obj("jeringa", 700, 700);
 
-const Obj = function(name, pri, stock){
-    this.name= name
-    this.pri= pri
-    this.stock = stock
+let list = [o1, o2, o3, o4, o5, o6, o7];
+if (localStorage.getItem("objetos")) {
+    list = JSON.parse(localStorage.getItem("objetos"));
 }
-let o1  = new Obj("alcohol",1500,30)
-let o2  = new Obj("mascara facial",15000,10)
-let o3  = new Obj("crema para manos",5000,20)
-let o4  = new Obj("aposito",500,500)
-let o5  = new Obj("Gasa",500,1000)
-let o6  = new Obj("aguja",600,700)
-let o7  = new Obj("jeringa",700,700)
 
-let list = [o1,o2,o3,o4,o5,o6,o7]
-function agregarObj(){
-    let name= prompt("ingresa el nombre del producto")
-    let pri = prompt("ingresa el precio del producto")
-    let stock = prompt("ingresa el stock del producto")
-    
-    if(isNaN(pri) || isNaN(stock) || name == ""){
-        alert("por favor ingrese valores validos")
-        return
-    }
-    pri = parseInt(pri);
-    stock = parseInt(stock);
+function agregarObj() {
+    Swal.fire({
+        title: `Agregar objeto`,
+        html: `<label>Nombre:</label> <input id="name-input" class="swal2-input" type="text" autofocus>
+        <label>Precio:</label><input id="pri-input" class="swal2-input" type="number" step="0.01">
+        <label>Stock:</label><input id="stock-input" class="swal2-input" type="number" step="1">`,
+        showCancelButton: true,
+        confirmButtonText: "Agregar",
+        cancelButtonText: "Cancelar",
+    }).then((result) => {
+        if (result.isConfirmed) {
+            let name = document.getElementById("name-input").value.trim();
+            let pri = parseFloat(document.getElementById("pri-input").value.trim());
+            let stock = parseInt(document.getElementById("stock-input").value.trim());
 
-    let p = new Obj (name,pri,stock)    
-    list.push(p)
-    console.log(list)
+            if (isNaN(pri) || isNaN(stock) || name === "") {
+                Swal.fire({
+                    icon: "error",
+                    title: "Error",
+                    text: "Por favor ingresa valores válidos",
+                });
+                return;
+            }
+
+            let objeto = new Obj(name, pri, stock);
+
+            if (list.some((elemento) => elemento.name === objeto.name)) {
+                Swal.fire({
+                    icon: "warning",
+                    title: "Advertencia",
+                    text: "El producto ya existe en la lista",
+                });
+                return;
+            }
+
+            list.push(objeto);
+            localStorage.setItem("objetos", JSON.stringify(list));
+
+            Swal.fire({
+                icon: "success",
+                title: "Objeto Agregado",
+                text: `Se agregó el objeto ${objeto.name} a la lista`,
+                timer: 3000,
+            });
+        }
+    });
 }
+
 function buscarObj() {
-    let buscar = prompt("Ingrese objeto a buscar")
-    let resultado = list.filter((x)=>x.name.toUpperCase().includes(buscar.toUpperCase()))
-    if (resultado.length>0){
-        console.table(resultado)
-    }else{
-        alert("no se encontro")
-    }
+    Swal.fire({
+        title: "Ingresa el producto que deseas buscar",
+        input: "text",
+        showCancelButton: true,
+        confirmButtonText: "Buscar",
+        showLoaderOnConfirm: true,
+        preConfirm: (palabraClave) => {
+            palabraClave = palabraClave.trim().toUpperCase();
+            let result = list.filter((objeto) =>
+                objeto.name.toUpperCase().includes(palabraClave)
+            );
+
+            if (result.length > 0) {
+                Swal.fire({
+                    title: "Este es el resultado de tu búsqueda",
+                    html:
+                        "<table><tr><th>Nombre</th><th>Precio</th><th>Stock</th></tr>" +
+                        result
+                            .map(
+                                (objeto) =>
+                                    `<tr><td>${objeto.name}</td><td>${objeto.pri}</td><td>${objeto.stock}</td></tr>`
+                            )
+                            .join("") +
+                        "</table>",
+                });
+            } else {
+                Swal.fire({
+                    title: `No se encontraron coincidencias`,
+                    icon: "error",
+                    confirmButtonText: `Ok`,
+                });
+            }
+        },
+    });
 }
+
+let agregar = document.getElementById("add");
+if (agregar) agregar.addEventListener("click", agregarObj);
+
+let filtrar = document.getElementById("filtrar");
+if (filtrar) filtrar.addEventListener("click", buscarObj);
